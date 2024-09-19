ba92d7a79f8ad564d58ba4ff0e2d348dfa8e4837
Sachin Dharmapurikar <sachin@dharmapurikar.in>
2024-09-20 04:11:04 +0530

    Fixing the test.py to use environment variables.

--- test.py
+++ test.py
@@ -6,14 +6,14 @@ from gpt_researcher.llm_provider.generic import GenericLLMProvider
 from gpt_researcher.utils.llm import get_llm
 
 # Set up environment variables. Change them as per your environment. Currently this code assumes you are running it from a docker container
-os.environ["LLM_PROVIDER"] = "ollama"
-os.environ["OLLAMA_BASE_URL"] = "http://host.docker.internal:11434"
-os.environ["FAST_LLM_MODEL"] = "llama3.1:latest"
+# os.environ["LLM_PROVIDER"] = "ollama"
+# os.environ["OLLAMA_BASE_URL"] = "http://host.docker.internal:11434"
+# os.environ["FAST_LLM_MODEL"] = "llama3.1:latest"
 
 # Create the GenericLLMProvider instance
 llm_provider = get_llm(
     "ollama",
-    base_url=os.environ["OLLAMA_BASE_URL"],
+    # base_url=os.environ["OLLAMA_BASE_URL"],
     model=os.environ["FAST_LLM_MODEL"],
     temperature=0.7,
     max_tokens=2000,
